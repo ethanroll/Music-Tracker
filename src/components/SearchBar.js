c50acbce20855ecfa8762a9be443499249a6f2d9
ethan caoile <ethancaoile1@gmail.com>
2026-05-06 00:26:00 -0700

    Started on project, added title/search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+function SearchBar(){
+    return(
+        <div>
+            <input type = "text" placeholder = "Search for an album..." />
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default SearchBar;
